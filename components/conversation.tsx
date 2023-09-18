6d0491a6d116569429754daf6bdec2fb5ae3670c
https://github.com/CleverDeveloper0929 <webapp.superdev@gmail.com>
2023-09-18 23:06:38 +0100

    update app6
    Co-authored-by: name <zhangmao0929@gmail.com>

--- components/conversation.tsx
+++ components/conversation.tsx
@@ -361,7 +361,7 @@ export default function Conversation({
                   alt="Remy Sharp"
                   src={`${
                     message.role === "assistant" || message.role === "system"
-                      ? "/headshots/jesus5.jpg"
+                      ? "/headshots/jesus6.jpg"
                       : ""
                   }`}
                 />
